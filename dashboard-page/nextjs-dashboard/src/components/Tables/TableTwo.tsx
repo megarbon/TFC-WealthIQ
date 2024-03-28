1d4b0183b19d71c88f08d69510b88d129287f35a
Melaku Garcia <56188626+megarbon@users.noreply.github.com>
2024-03-28 01:22:52 +0100

    portfolio types and table unfinished

--- dashboard-page/nextjs-dashboard/src/components/Tables/TableTwo.tsx
+++ dashboard-page/nextjs-dashboard/src/components/Tables/TableTwo.tsx
@@ -1,38 +1,39 @@
+import React from "react";
 import Image from "next/image";
-import { Product } from "@/types/product";
+import { Portfolio } from "@/types/portfolio";
+import { investment } from "@/types/investment";
+import { asset } from "@/types/asset";
 
-const productData: Product[] = [
+const portfolioData: Portfolio[] = [
   {
-    image: "/images/product/product-01.png",
-    name: "Apple Watch Series 7",
-    category: "Electronics",
-    price: 296,
-    sold: 22,
-    profit: 45,
+    id: 1,
+    investments: [
+      {
+        id: 1,
+        asset: {
+          id: 1,
+          name: "Bitcoin",
+          symbol: "BTC",
+          market: "Crypto",
+        },
+        amount: 1000,
+      },
+    ],
   },
   {
-    image: "/images/product/product-02.png",
-    name: "Macbook Pro M1",
-    category: "Electronics",
-    price: 546,
-    sold: 12,
-    profit: 125,
-  },
-  {
-    image: "/images/product/product-03.png",
-    name: "Dell Inspiron 15",
-    category: "Electronics",
-    price: 443,
-    sold: 64,
-    profit: 247,
-  },
-  {
-    image: "/images/product/product-04.png",
-    name: "HP Probook 450",
-    category: "Electronics",
-    price: 499,
-    sold: 72,
-    profit: 103,
+    id: 2,
+    investments: [
+      {
+        id: 2,
+        asset: {
+          id: 2,
+          name: "Microsoft",
+          symbol: "MSFT",
+          market: "NASDAQ",
+        },
+        amount: 5,
+      },
+    ],
   },
 ];
 
@@ -41,64 +42,54 @@ const TableTwo = () => {
     <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
       <div className="px-4 py-6 md:px-6 xl:px-7.5">
         <h4 className="text-xl font-semibold text-black dark:text-white">
-          Top Products
+          Portfolio
         </h4>
       </div>
 
       <div className="grid grid-cols-6 border-t border-stroke px-4 py-4.5 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
         <div className="col-span-3 flex items-center">
-          <p className="font-medium">Product Name</p>
+          <p className="font-medium">Asset Name</p>
         </div>
         <div className="col-span-2 hidden items-center sm:flex">
-          <p className="font-medium">Category</p>
+          <p className="font-medium">Symbol</p>
         </div>
         <div className="col-span-1 flex items-center">
-          <p className="font-medium">Price</p>
+          <p className="font-medium">Market</p>
         </div>
         <div className="col-span-1 flex items-center">
-          <p className="font-medium">Sold</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium">Profit</p>
+          <p className="font-medium">Amount</p>
         </div>
       </div>
 
-      {productData.map((product, key) => (
+      {portfolioData.map((portfolio, key) => (
         <div
           className="grid grid-cols-6 border-t border-stroke px-4 py-4.5 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5"
           key={key}
         >
-          <div className="col-span-3 flex items-center">
-            <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-              <div className="h-12.5 w-15 rounded-md">
-                <Image
-                  src={product.image}
-                  width={60}
-                  height={50}
-                  alt="Product"
-                />
+          {portfolio.investments.map((investment, index) => (
+            <React.Fragment key={index}>
+              <div className="col-span-3 flex items-center">
+                <p className="text-sm text-black dark:text-white">
+                  {investment.asset.name}
+                </p>
               </div>
-              <p className="text-sm text-black dark:text-white">
-                {product.name}
-              </p>
-            </div>
-          </div>
-          <div className="col-span-2 hidden items-center sm:flex">
-            <p className="text-sm text-black dark:text-white">
-              {product.category}
-            </p>
-          </div>
-          <div className="col-span-1 flex items-center">
-            <p className="text-sm text-black dark:text-white">
-              ${product.price}
-            </p>
-          </div>
-          <div className="col-span-1 flex items-center">
-            <p className="text-sm text-black dark:text-white">{product.sold}</p>
-          </div>
-          <div className="col-span-1 flex items-center">
-            <p className="text-sm text-meta-3">${product.profit}</p>
-          </div>
+              <div className="col-span-2 hidden items-center sm:flex">
+                <p className="text-sm text-black dark:text-white">
+                  {investment.asset.symbol}
+                </p>
+              </div>
+              <div className="col-span-1 flex items-center">
+                <p className="text-sm text-black dark:text-white">
+                  {investment.asset.market}
+                </p>
+              </div>
+              <div className="col-span-1 flex items-center">
+                <p className="text-sm text-black dark:text-white">
+                  {investment.amount}
+                </p>
+              </div>
+            </React.Fragment>
+          ))}
         </div>
       ))}
     </div>
--- user-auth-service/AuthenticatedBackend/src/main/java/com/backendwealthiq/controllers/UserController.java
+++ user-auth-service/AuthenticatedBackend/src/main/java/com/backendwealthiq/controllers/UserController.java
@@ -5,21 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/user")
 @CrossOrigin("*")
 public class UserController {
 
     @GetMapping("/")
-    public String helloUserController(){
+    public String helloUserController() {
         return "User access level";
     }
 
     @GetMapping("/secret")
     public String privateEndpoint() {
         // Redirect to a static HTML page
-        return "redirect:http://localhost:4321/private.html";
+        return "redirect:http://localhost:3000/";
     }
-    
+
 }
