147da7232a1a1795c917a2f31aa71c3f23ec78b0
ggaarryy4 <145008575+ggaarryy4@users.noreply.github.com>
2024-06-06 16:56:22 +0200

    limpieza de dashboard y creacion pestaña libros
    limpiezaz de archivos que no se usan en el dashboard y creacion de pestaña de libros

---
+++ nextjs-dashboard/src/app/books/page.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import Library from './library';
+import booksData from './library.json';
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+
+export const metadata: Metadata = {
+  title: "Books",
+  description:
+    "Books about investing",
+};
+
+const books = () => {
+  return (
+    <DefaultLayout>
+       <Breadcrumb pageName="Books" />
+      <div className="text-center">
+      <header className="bg-[#1C2434] p-5 text-white text-4xl">
+        <h1>Book Library</h1>
+      </header>
+      <Library data={booksData} />
+    </div>
+    </DefaultLayout>
+    
+  );
+}
+
+export default books;
